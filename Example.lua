b864b4510c2501efac7479e47c3e314b4754ba1d
DataStore²_Genius² <lk6394785@gmail.com>
2024-11-20 06:57:00 -0900

    Update Example.lua
    Update to shortE example
    but u still can use
    shortE(12345) -- but shows up as 1.2345e4

--- Example.lua
+++ Example.lua
@@ -5,6 +5,8 @@ local first = NumberFormat.add(1.16518, 1.84894)
 print(first) -- output is just 3.01412
 --local first = NumberFormat.add(1.16518, 1.84894, true) -- output is 3.01
 --same with other calculations
-local shortE = NumberFormat.shortE(12000) -- non-canNotation will preset toNotation at 1e6 which is 1,000*1,000
+local shortE = NumberFormat.shortE(12345, true) -- non-canNotation will preset toNotation at 1e6 which is 1,000*1,000
+--new push is canNotation on the shortE so u can correct 1.2345e4 to 1.23e4
+--and also shortE setup for short without calling short but if u want a good Notation set Notation past 1e100
 print(shortE)-- also pre-formats Comma if its under 1m so output is 12,000 if u want to do 1m after Comma set the canNotation to 1e30
 --thats pretty much it
