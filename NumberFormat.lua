4d68bb27c407ef97b9914a75e85958c949df5214
DataStore²_Genius² <lk6394785@gmail.com>
2024-11-21 18:01:04 -0900

    Update NumberFormat.lua
    if may look like a removed a long of things but i made it shorter aka compact but still the same thing

--- NumberFormat.lua
+++ NumberFormat.lua
@@ -1,8 +1,6 @@
 local first = {"", "U","D","T","Qd","Qn","Sx","Sp","Oc","No"}
 local second = {"", "De","Vt","Tg","qg","Qg","sg","Sg","Og","Ng"}
 local third = {'', 'Ce'}
-local Players = game:GetService('Players')
-local Player = Players.LocalPlayer
 
 local Number = {}
 
@@ -32,69 +30,53 @@ end
 
 function Number.add(val1, val2, canRound: boolean?)
 	local value = val1+val2
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value)	end
 	return value
 end
 
 function Number.div(val1, val2, canRound: boolean?)
 	local value = val1/val2
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value)end
 	return value
 end
 
 function Number.mul(val1, val2, canRound: boolean?)
 	local value = val1*val2
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value) end
 	return value
 end
 
 function Number.sub(val1, val2, canRound: boolean?)
 	val1 = val1 - val2
 	if val1 <= 0 then return 0 end
-	if canRound then
-		return Number.floor(val1)
-	end
+	if canRound then	return Number.floor(val1) end
 	return val1
 end
 
 function Number.log(value, canRound: boolean?)
 	value = math.log(value)
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value) end
 	return value
 end
 
 function Number.logx(val1, val2, canRound: boolean?)
 	local value = math.log(val1, val2)
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value) end
 	return value
 end
 
-function Number.log10(val1)
-	return Number.logx(val1, 10)
+function Number.log10(val1, canRound: boolean?)
+	return Number.logx(val1, 10, canRound)
 end
 
 function Number.pow(val1, val2, canRound: boolean?)
 	val1 = val1^val2
-	if canRound then return Number.floor(val1) end
-	return val1
+	if canRound then return Number.floor(val1) end return val1
 end
 
 function Number.clamp(value: number, min: number, max: number)
-	if min > max then
-		min, max = max, min
-	end
-	if value < min then return min
-	elseif value > max then return max end
+	if Number.me(min, max) then min, max = max, min end
+	if Number.le(value, min) then return min elseif Number.me(value, max) then return max end
 	return value
 end
 
@@ -108,9 +90,7 @@ end
 
 function Number.mod(val1, val2, canRound: boolean?)
 	local value = val1 % val2
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value) end
 	return value
 end
 
@@ -128,8 +108,7 @@ function Number.Comma(value)
 		value = math.floor(value)
 		local format = tostring(value)
 		format = format:reverse():gsub('(%d%d%d)', '%1,'):reverse()
-		if format:sub(1, 1) == ',' then format = format:sub(2) end
-		return format
+		if format:sub(1, 1) == ',' then format = format:sub(2) end return format
 	end
 	return value
 end
@@ -147,20 +126,15 @@ end
 function Number.toNotation(value, canRound: boolean?)
 	local toTable = Number.toTable(value)
 	local man, exp = toTable[1], toTable[2]
-	if canRound then
-		return Number.floor(man) .. 'e' .. exp
-	end
+	if canRound then	return Number.floor(man) .. 'e' .. exp end
 	return man .. 'e' .. exp
 end
 
 function suffixPart(index)
 	local hun = math.floor(index/100)
 	index = index%100
-	local ten = math.floor(index/10)
-	local one = index%10
-	return (first[one+1] or '') ..
-		(second[ten+1] or '') .. 
-		(third[hun+1] or '')
+	local ten, one = math.floor(index/10), index % 10
+	return (first[one+1] or '') ..(second[ten+1] or '') .. (third[hun+1] or '')
 end
 
 function Number.short(value)
@@ -171,19 +145,13 @@ function Number.short(value)
 	if ind > 101 then return 'inf' end
 	local rm = exp%3
 	man = man*10^rm
-	if ind == 0 then
-		return string.format('%dk', man)
-	elseif ind == 1 then
-		return string.format('%dm', man)
-	end
+	if ind == 0 then return string.format('%dk', man)	elseif ind == 1 then	return string.format('%dm', man)	end
 	return man .. suffixPart(ind)
 end
 
 function Number.shortE(value: number, canRound: boolean?, canNotation: number?): 'Notation will automatic preset but if u want one smaller do it as 1e3'
 	canNotation = canNotation or 1e6
-	if math.abs(value) >= canNotation then
-		return Number.toNotation(value, canRound):gsub('nane','')
-	end
+	if math.abs(value) >= canNotation then return Number.toNotation(value, canRound):gsub('nane','')	end
 	return Number.short(value)
 end
 
@@ -202,22 +170,14 @@ function Number.CorrectTime(value: number)
 	local seconds = value % 60
 	local result = ""
 	local function appendTime(unit, label)
-		if unit > 0 then
-			result = result .. string.format(':%d%s', unit, label)
-		end
+		if unit > 0 then	result = result .. string.format(':%d%s', unit, label)	end
 	end
 	if days > 0 then
-		result = string.format('%dd', days)
-		appendTime(hours, 'h')
-		appendTime(minutes, 'm')
-		appendTime(seconds, 's')
+		result = string.format('%dd', days) appendTime(hours, 'h')	appendTime(minutes, 'm') appendTime(seconds, 's') 
 	elseif hours > 0 then
-		result = string.format('%dh', hours)
-		appendTime(minutes, 'm')
-		appendTime(seconds, 's')
+		result = string.format('%dh', hours)	appendTime(minutes, 'm') appendTime(seconds, 's')
 	elseif minutes > 0 then
-		result = string.format('%dm', minutes)
-		appendTime(seconds, 's')
+		result = string.format('%dm', minutes) 	appendTime(seconds, 's')
 	else
 		result = string.format('%ds', seconds)
 	end
@@ -226,9 +186,7 @@ end
 
 function Number.percent(part, total, canRound: boolean?)
 	local value = (part / total) * 100
-	if canRound then
-		return Number.floor(value)
-	end
+	if canRound then	return Number.floor(value) end
 	if value < 0.001 then return '0%' end
 	return value .. '%'
 end
@@ -237,8 +195,14 @@ function Number.Changed(value, callBack: (property: string) -> ())
 	value.Changed:Connect(callBack)
 end
 
+function Number.Concat(value, canRound: boolean?, canNotation: number?)
+	canNotation = canNotation or 1e6
+	if value >= canNotation then return Number.shortE(value, canRound, canNotation) end
+	return Number.Comma(value)
+end
+
 Number.__index = Number
-function Number.GetValue(valueName)
+function Number.GetValue(valueName, Player: Player)
 	local self = setmetatable({}, Number)
 	for _, names in pairs(Player:GetDescendants()) do
 		if names.Name == valueName then
